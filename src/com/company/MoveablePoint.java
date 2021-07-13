228a9a8582d091199f966829059c0ec64b72c60c
TrungDucMai <trungmaiduc196@gmail.com>
2021-07-13 09:25:33 +0700

    'taomoi'

---
+++ src/com/company/MoveablePoint.java
@@ -0,0 +1,53 @@
+package com.company;
+
+public class MoveablePoint extends Point {
+    float xSpeed = 0.0f;
+    float ySpeed = 0.0f;
+
+    public MoveablePoint() {
+    }
+
+    public MoveablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MoveablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float getxSpeed() {
+        return xSpeed;
+    }
+
+    public void setxSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getySpeed() {
+        return ySpeed;
+    }
+
+    public void setySpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float[] getSpeed() {
+        return new float[]{xSpeed, ySpeed};
+    }
+//    public Move()
+    @Override
+    public String toString() {
+        return "MoveablePoint{" +
+                "x,y=" + getX() + " "+ getY() +
+                ", speed " + xSpeed +" "+ ySpeed +
+                '}';
+    }
+}
---
+++ src/com/company/MoveablePointTest.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class MoveablePointTest {
+    public static void main(String[] args) {
+        MoveablePoint moveablePoint = new MoveablePoint( 10, 32);
+        System.out.println(moveablePoint);
+        moveablePoint = new MoveablePoint(23,44,23,53);
+        System.out.println(moveablePoint);
+    }
+}
